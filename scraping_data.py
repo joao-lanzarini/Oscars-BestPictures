52aceb1aa6f258b77f84e7cda140d88e3e6b8d5d
joao-lanzarini <joaovitorlsilva02@gmail.com>
2021-10-26 13:02:06 -0300

    Update scraping_data.py
    Handling the html and getting title and year data from each movie

--- scraping_data.py
+++ scraping_data.py
@@ -6,10 +6,20 @@ request = requests.get(url)
 html_page = bs(request.content, 'html.parser')
 
 title_divs = html_page.find_all(class_='lister-item-header')
-years = html_page.find_all(class_='lister-item-year text-muted unbold')
 
-for i in title_divs[3]:
-    print(i)
+title=[]
+year=[]
 
-print(years[17].get_text())
+for i in title_divs[0:8]:
+    c=0
+    for a in i:
+        if c == 3:
+            title.append(a.get_text())
+        elif c == 5:
+            year.append(a.get_text())
+
+        c+=1
+
+print(title)
+print(year)
 
